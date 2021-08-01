2eab604098c93c388f8dafcbd9ac59d29facfb74
tamvv <vovantam.dev@gmail.com>
2021-08-01 11:36:52 +0700

    Update venv and urls

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
 from .views import SignupView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
